6d1249aaf26ea3b95a7b9cfe6bdbbd84799efa0f
Farouk Bakre <faroukbakre@yahoo.com>
2023-04-03 10:27:39 +0100

    First changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('phoneNumber')->unique();
+            $table->string('emailAddress')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->date('dateOfBirth');
+            $table->string('pictureUrl');
+            $table->string('role');
+            $table->mediumtext('address');
             $table->timestamps();
         });
     }
